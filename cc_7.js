fb2e95447fb2a7a5b196049b2a2f4430b72637fd
marianachacinn <marianachacin@usf.edu>
2025-02-16 15:56:03 -0500

    Task 3 - Customer Loyalty Discount.

--- cc_7.js
+++ cc_7.js
@@ -13,3 +13,12 @@ const calculateHourlyWage = function(salary, hoursPerWeek) { // formula for wage
 };
 calculateHourlyWage(52000, 40); //expected: $25.00
 calculateHourlyWage(75000, 35); //expected: $41.21
+
+console.log ('Task 3: Arrow Function') //task 3
+const calculateLoyaltyDiscount = (amount, years) => { //function for discount
+    let discountRate = years >= 5 ? 0.15 : years >= 3 ? 0.10 : 0.05; //more than 5 years: 15%, more than 3: 10%, less than 3: 5%
+    let discountedPrice = amount * (1 - discountRate); //subsracts discount from the amount
+    console.log(`Discounted Price: $${discountedPrice.toFixed(2)}`);
+};
+calculateLoyaltyDiscount(100, 6); // Expected output: "Discounted Price: $85.00"
+calculateLoyaltyDiscount(200, 2); // Expected output: "Discounted Price: $190.00"
